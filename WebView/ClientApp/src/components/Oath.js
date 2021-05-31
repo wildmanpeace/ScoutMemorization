016e0324d8c278af851a597444d883abfcc8e8ed
Alex Wild <alex.ladell.wild@gmail.com>
2021-05-30 19:08:13 -0600

    Got moving items working!

--- WebView/ClientApp/src/components/Oath.js
+++ WebView/ClientApp/src/components/Oath.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { Container, Paper } from "@material-ui/core"
 
-const OathLists = {
-    ScabbledOath: {
-      id: "Scabbled Oath",
-    list:[
-    {id: 1, text: "On my honor"},
-    {id: 2, text: "I will do my best"},
-    {id: 3, text: "To do duty"},
-    {id: 4, text: "To God"},
-    {id: 5, text: "And my country"},
-    {id: 6, text: "To obey the Scout Law"},
-    {id: 7, text: "To help other people at all time"},
-    {id: 8, text: "to keep myself"},
-    {id: 9, text: "physically strong"},
-    {id: 10, text: "mentally awake"},
-    {id: 11, text: "and morally straight"}
-  ]
-},
-    FinalOath: {
-      id: "Final Oath",
-      list:[]
-    }
-};
+const OathLists = () => [
+  { id: "id-3", text: "To do duty" },
+  { id: "id-8", text: "to keep myself" },
+  { id: "id-2", text: "I will do my best" },
+  { id: "id-4", text: "To God" },
+  { id: "id-1", text: "On my honor" },
+  { id: "id-11", text: "and morally straight" },
+  { id: "id-5", text: "And my country" },
+  { id: "id-10", text: "mentally awake" },
+  { id: "id-9", text: "physically strong" },
+  { id: "id-6", text: "To obey the Scout Law" },
+  { id: "id-7", text: "To help other people at all time" }
+];
 
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
@@ -38,14 +30,15 @@ const reorder = (list, startIndex, endIndex) => {
       constructor(props){
         super(props);
         this.state = {
-          originalList: OathLists.ScabbledOath.list
+          originalList: OathLists(),
+          counter: 0
         };
         this.onDragEnd = this.onDragEnd.bind(this)
+        this.checkOrder = this.checkOrder.bind(this)
       }
 
       onDragEnd(result){
-        if(!result.destination)
-        return;
+        if(!result.destination) return;
 
         const originalList = reorder(
           this.state.originalList,
@@ -53,27 +46,53 @@ const reorder = (list, startIndex, endIndex) => {
           result.destination.index
         )
 
-        this.state({originalList});
-      }      
+        this.setState({
+          originalList,
+          counter: this.state.counter + 1
+        });
+
+        //check order
+        if(this.checkOrder(this.state.originalList)){
+          console.log("finished")
+        }
+        //if order is correct display happy message :)
+
+      }
+      
+      checkOrder(list){
+        const correctOrder = ["1","2","3","4","5","6","7","8","9","10","11"];
+        const listIDs = list.map((item => item.id = item.id.replace("id-", "")));
+        
+        if(correctOrder.values === listIDs.values){
+          return true;
+        }else{
+          return false;
+        }
+
+      }
     
       render () {
         return (
+          <>
+          <h2>Scout Oath</h2>
+          <p>The Scout Oath is out of order! Move the cards and place them in the correct order</p>
+          <Container>
           <DragDropContext onDragEnd={this.onDragEnd}>
           <Droppable droppableId="droppable">
-            {(provided, snapshot) => (
+            {(provided) => (
               <div
                 {...provided.droppableProps}
                 ref={provided.innerRef}
               >
                 {this.state.originalList.map((item, index) => (
-                  <Draggable key={item.id} draggableId={item.id} index={index}>
-                    {(provided, snapshot) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index} >
+                    {(provided) => (
                       <div
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                       >
-                        {item.text}
+                        <Paper elevation={6}>{item.text}</Paper>
                       </div>
                     )}
                   </Draggable>
@@ -83,6 +102,11 @@ const reorder = (list, startIndex, endIndex) => {
             )}
           </Droppable>
         </DragDropContext>
+        <br />
+        <div>Counter: {this.state.counter}</div>
+        </Container>
+
+        </>
           )
       };
     }
